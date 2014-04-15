aa7ab21c76cc1ad8e66f3028b5ff061a4fde64b3
Sam W <sweldemariam@gmail.com>
2014-04-15 04:38:40 -0500

    :cheers: test

--- cheers.rb
+++ cheers.rb
@@ -1,14 +1,10 @@
-# Build on the results of our in-class exploration to output, for example:
-# Give me an... A
-# Give me a... B
-# Give me a... B
-# Give me a... Y
-# ABBY’s just GRAND!
-# When given the input of “Abby”. Note: the “a” vs. “an”
+name = gets.chomp.downcase
 
-# puts "I got these arguments: #{ARGV.join(", ")}"
-
-name = ARGV[0]
-name.each_char do |char|
-  puts "Give me a.. #{char.upcase}"
+if name.length > 0 && name.to_i == 0
+  puts "#{name.upcase}'s just GRAND!"
+  name.each_char do |char| char.gsub(/[aeiouy]/, "").length == 0 ? word = "an" : word = "a"
+    puts "Give me #{word}.. #{char.upcase}"
+  end
+else
+  puts "Nothing passed!"
 end
--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -3,11 +3,111 @@ require_relative 'helper'
 class TestCheersIntegration < MiniTest::Unit::TestCase
 
   def test_a_name_with_no_vowels
-    shell_output = `ruby cheers.rb brt`
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "BRT"
+      pipe.close_write
+      shell_output = pipe.read
+    end
     expected_output = <<EOS
+BRT's just GRAND!
 Give me a.. B
 Give me a.. R
 Give me a.. T
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_a_name_with_volwels
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "BART"
+      pipe.close_write
+      shell_output = pipe.read
+  end
+    expected_output = <<EOS
+BART's just GRAND!
+Give me a.. B
+Give me an.. A
+Give me a.. R
+Give me a.. T
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_a_lowercase_letters_output
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "sami"
+      pipe.close_write
+      shell_output = pipe.read
+  end
+    expected_output = <<EOS
+SAMI's just GRAND!
+Give me a.. S
+Give me an.. A
+Give me a.. M
+Give me an.. I
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_combination_of_upper_and_lowercase_letters
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "BaRt"
+      pipe.close_write
+      shell_output = pipe.read
+  end
+    expected_output = <<EOS
+BART's just GRAND!
+Give me a.. B
+Give me an.. A
+Give me a.. R
+Give me a.. T
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_empty_string_input
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts ""
+      pipe.close_write
+      shell_output = pipe.read
+  end
+    expected_output = <<EOS
+Nothing passed!
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_if_numbers_are_the_input
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "123"
+      pipe.close_write
+      shell_output = pipe.read
+  end
+    expected_output = <<EOS
+Nothing passed!
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_if_numbers_and_letters_are_the_input
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "123a"
+      pipe.close_write
+      shell_output = pipe.read
+  end
+    expected_output = <<EOS
+123A's just GRAND!
+Give me a.. 1
+Give me a.. 2
+Give me a.. 3
+Give me an.. A
 EOS
     assert_equal expected_output, shell_output
   end
